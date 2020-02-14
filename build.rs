2611c0f853add2e621496aa2592c5dc60da7518d
Thom Wiggers <thom@thomwiggers.nl>
2020-02-14 13:18:04 +0100

    update

--- build.rs
+++ build.rs
@@ -4,8 +4,10 @@ use std::path::Path;
 
 fn main() {
     let csidh_path = Path::new("constant-csidh-c-implementation");
-    let csidh_files: Vec<_> = ["rng.c", "mont.c", "fp.S", "u512.S", "csidh.c", "libcsidh.c"].into_iter().map(|f|
-        csidh_path.clone().join(f)).collect();
+    let csidh_files: Vec<_> = ["rng.c", "mont.c", "fp.S", "u512.S", "csidh.c", "libcsidh.c"]
+        .iter()
+        .map(|f| csidh_path.clone().join(f))
+        .collect();
     cc::Build::new()
         .flag("-std=c99")
         .flag("-O3")
--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,40 @@ use libc;
 
 const NUM_PRIMES: usize = 74;
 
+pub const PUBLIC_KEY_LEN: usize = 64;
+pub const PRIVATE_KEY_LEN: usize = NUM_PRIMES;
+
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct CSIDHPrivateKey {
     e: [i8; NUM_PRIMES],
 }
 
+pub type PublicKey = CSIDHPublicKey;
+pub type SecretKey = CSIDHPrivateKey;
+
 impl CSIDHPrivateKey {
     fn new() -> Self {
         CSIDHPrivateKey { e: [0i8; 74] }
+    }
+
+    pub fn generate() -> Self {
+        generate_private()
+    }
+
+    pub fn as_slice(&self) -> &[u8] {
+        unsafe { &*((&self.e[..]) as *const _ as *const [u8]) }
+    }
+
+    pub fn as_bytes(&self) -> &[u8] {
+        self.as_slice()
+    }
+
+    pub fn from_bytes(bytes: &[u8]) -> Self {
+        let i8bytes: &[i8] = unsafe { &*(bytes as *const _ as *const [i8]) };
+        let mut sk = CSIDHPrivateKey::new();
+        sk.e.copy_from_slice(i8bytes);
+        sk
     }
 }
 
@@ -24,38 +49,74 @@ impl CSIDHPublicKey {
     fn new() -> Self {
         CSIDHPublicKey { c: [0u8; 64] }
     }
+
+    pub fn from_private(private: &CSIDHPrivateKey) -> Self {
+        public_from_private(private)
+    }
+
+    pub fn as_slice(&self) -> &[u8] {
+        &self.c
+    }
+
+    pub fn as_bytes(&self) -> &[u8] {
+        self.as_slice()
+    }
+
+    pub fn from_bytes(bytes: &[u8]) -> Self {
+        let mut pk = Self::new();
+        pk.c.copy_from_slice(bytes);
+        pk
+    }
 }
 
 #[link(name = "csidh")]
 extern "C" {
     pub fn csidh_generate(key: *mut CSIDHPrivateKey) -> libc::c_int;
-    pub fn csidh_derive(out: *mut CSIDHPublicKey, base: *const CSIDHPublicKey, key: *const CSIDHPrivateKey) -> libc::c_int;
+    pub fn csidh_derive(
+        out: *mut CSIDHPublicKey,
+        base: *const CSIDHPublicKey,
+        key: *const CSIDHPrivateKey,
+    ) -> libc::c_int;
     pub static csidh_base: CSIDHPublicKey;
 }
 
-pub fn keypair() -> (CSIDHPublicKey, CSIDHPrivateKey) {
-    let mut pk = CSIDHPublicKey::new();
+pub fn generate_private() -> CSIDHPrivateKey {
     let mut sk = CSIDHPrivateKey::new();
+
     unsafe {
         csidh_generate(&mut sk as *mut CSIDHPrivateKey);
-        csidh_derive(&mut pk as *mut CSIDHPublicKey,
-                     &csidh_base as *const CSIDHPublicKey,
-                     &sk as *const CSIDHPrivateKey);
     }
+    sk
+}
 
-    (pk, sk)
+pub fn public_from_private(key: &CSIDHPrivateKey) -> CSIDHPublicKey {
+    let mut pk = CSIDHPublicKey::new();
+    unsafe {
+        csidh_derive(
+            &mut pk as *mut CSIDHPublicKey,
+            &csidh_base as *const CSIDHPublicKey,
+            key as *const CSIDHPrivateKey,
+        );
+    }
+    pk
+}
+
+pub fn keypair() -> (CSIDHPublicKey, CSIDHPrivateKey) {
+    let sk = generate_private();
+    (public_from_private(&sk), sk)
 }
 
 pub fn agreement(theirs: &CSIDHPublicKey, ours: &CSIDHPrivateKey) -> [u8; 64] {
     let mut agreed = CSIDHPublicKey::new();
     unsafe {
-        csidh_derive(&mut agreed as *mut CSIDHPublicKey,
-                     theirs as *const CSIDHPublicKey,
-                     ours as *const CSIDHPrivateKey);
+        csidh_derive(
+            &mut agreed as *mut CSIDHPublicKey,
+            theirs as *const CSIDHPublicKey,
+            ours as *const CSIDHPrivateKey,
+        );
     }
     agreed.c
 }
-
 
 #[cfg(test)]
 mod test {
