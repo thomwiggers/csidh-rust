42d9dbd1f83e74523df1df6b0d336a4f1dcadf41
Thom Wiggers <thom@thomwiggers.nl>
2023-03-22 17:50:41 +0100

    reorganize benchmark

--- benches/ctidh.rs
+++ benches/ctidh.rs
@@ -2,11 +2,13 @@ use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use csidh_rust::*;
 use paste::paste;
 
-pub fn criterion_benchmark(c: &mut Criterion) {
+pub fn criterion_benchmark(crit: &mut Criterion) {
+    let mut group = crit.benchmark_group("ctidh");
+    group.measurement_time(std::time::Duration::new(90, 0));
     macro_rules! bench {
         ($c: ident, $size: literal) => {
             let size: u32 = $size;
-            $c.bench_function(&format!("ctidh {size} keypair"), |b| {
+            $c.bench_function(&format!("keypair/{size}"), |b| {
                 b.iter(|| {
                     paste! {
                         [< ctidh $size >]::keypair()
@@ -14,7 +16,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
                 })
             });
             let keys = paste! { [< ctidh $size >]::keypair() };
-            $c.bench_with_input(BenchmarkId::new("csidh action", "512"), &keys, |b, &keys| {
+            $c.bench_with_input(BenchmarkId::new("action", size), &keys, |b, &keys| {
                 b.iter(|| {
                     paste! {
                         [<ctidh $size>]::agreement(&keys.0, &keys.1)
@@ -23,10 +25,10 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             })
         };
     }
-    bench!(c, 511);
-    bench!(c, 512);
-    bench!(c, 1024);
-    bench!(c, 2048);
+    bench!(group, 511);
+    bench!(group, 512);
+    bench!(group, 1024);
+    bench!(group, 2048);
 }
 
 criterion_group!(benches, criterion_benchmark);
